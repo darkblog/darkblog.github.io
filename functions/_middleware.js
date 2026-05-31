26bc121805d119e2f99e186c7d3e342c1eab3386
darkblog <ehsanchhry@gmail.com>
2026-06-01 03:05:54 +0330

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,11 +3,22 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const path = url.pathname;
   
-  const numericPostPattern = /^\/(\d+)\/?$/;
-  const match = path.match(numericPostPattern);
+  const bioPattern = /^\/biography\/(\d+)\/?$/;
+  const bioMatch = path.match(bioPattern);
   
-  if (match) {
-    const postId = match[1];
+  if (bioMatch) {
+    const postId = bioMatch[1];
+    return Response.redirect(
+      `https://www.pasgah.org/blog/${postId}`,
+      301
+    );
+  }
+  
+  const numericPostPattern = /^\/(\d+)\/?$/;
+  const numericMatch = path.match(numericPostPattern);
+  
+  if (numericMatch) {
+    const postId = numericMatch[1];
     return Response.redirect(
       `https://www.pasgah.org/blog/${postId}`,
       301
