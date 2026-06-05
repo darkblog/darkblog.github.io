8a8ed2f87237d40ec1f5b20f81755b3839c71bd7
darkblog <ehsanchhry@gmail.com>
2026-06-05 08:13:42 +0330

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,11 +1,11 @@
 export async function onRequest(context) {
-  const { request, env } = context;
+  const { request, next } = context;
   const url = new URL(request.url);
   const path = url.pathname;
-
+  
   const bioPattern = /^\/biography\/(\d+)\/?$/;
   const bioMatch = path.match(bioPattern);
-
+  
   if (bioMatch) {
     const postId = bioMatch[1];
     return Response.redirect(
@@ -13,10 +13,10 @@ export async function onRequest(context) {
       301
     );
   }
-
+  
   const numericPostPattern = /^\/(\d+)\/?$/;
   const numericMatch = path.match(numericPostPattern);
-
+  
   if (numericMatch) {
     const postId = numericMatch[1];
     return Response.redirect(
@@ -24,7 +24,7 @@ export async function onRequest(context) {
       301
     );
   }
-
+  
   if (path !== "/" && path.endsWith("/")) {
     const newPath = path.slice(0, -1);
     return Response.redirect(
@@ -32,19 +32,6 @@ export async function onRequest(context) {
       301
     );
   }
-
-  const modifiedRequest = new Request(`https://${url.hostname}${path}${url.search}`, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-    redirect: 'manual'
-  });
-
-  const response = await env.ASSETS.fetch(modifiedRequest);
-
-  if (response.status === 404) {
-    return Response.redirect('https://www.pasgah.org/blog/1', 301);
-  }
-
-  return response;
+  
+  return next();
 }
