d887d42802965e69c3fc6f164cd53d3a4ec3b78d
darkblog <ehsanchhry@gmail.com>
2026-06-12 04:00:56 +0330

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,31 +2,36 @@ export async function onRequest(context) {
   const { request, next } = context;
   const url = new URL(request.url);
   const path = url.pathname;
-
-  if (path === '/blog.xml' || path === '/feed.xml') {
-    return Response.redirect('https://www.pasgah.org/sitemap.xml', 301);
-  }
-
+  
   const bioPattern = /^\/biography\/(\d+)\/?$/;
   const bioMatch = path.match(bioPattern);
-
+  
   if (bioMatch) {
     const postId = bioMatch[1];
-    return Response.redirect(`https://www.pasgah.org/blog/${postId}`, 301);
+    return Response.redirect(
+      `https://www.pasgah.org/blog/${postId}`,
+      301
+    );
   }
-
+  
   const numericPostPattern = /^\/(\d+)\/?$/;
   const numericMatch = path.match(numericPostPattern);
-
+  
   if (numericMatch) {
     const postId = numericMatch[1];
-    return Response.redirect(`https://www.pasgah.org/blog/${postId}`, 301);
+    return Response.redirect(
+      `https://www.pasgah.org/blog/${postId}`,
+      301
+    );
   }
-
-  if (path !== '/' && path.endsWith('/')) {
+  
+  if (path !== "/" && path.endsWith("/")) {
     const newPath = path.slice(0, -1);
-    return Response.redirect(`https://www.pasgah.org${newPath}`, 301);
+    return Response.redirect(
+      `https://www.pasgah.org${newPath}`,
+      301
+    );
   }
-
+  
   return next();
 }
